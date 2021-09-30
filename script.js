d226a078ecdf84c2a504bee3f167be8d38e70bfd
Chris Hunley <cshunley21@gmail.com>
2021-09-30 09:55:40 -0500

    fixed bonus

--- script.js
+++ script.js
@@ -27,3 +27,26 @@ droids["assassin"] = "IG-11";
 console.log(droids);
 
 //4
+console.log(starWarsVillains[0][6]);
+
+//5
+console.log(sithLords.slice(-2, -1))
+
+//6
+const starWarsMovies = [
+    {
+      episodeOne: "The Phantom Menace",
+      episodeTwo: "Attack of the Clones",
+      episodeThree: "Revenge of the Sith"},
+    {
+      episodeFour: "A New Hope",
+      episodeFive: "The Empire Strikes Back",
+      episodeSix: "Return of the Jedi"
+    },
+    {
+      episodeSeven: "The Force Awakens",
+      episodeEight: "The Last Jedi",
+      episodeNine: "The Rise of Skywalker"
+    }
+  ];
+starWarsMovies.splice(1, 0, "Solo", "Rogue One");
